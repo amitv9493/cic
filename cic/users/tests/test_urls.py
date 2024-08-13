73bb22d1bd0a8fff7d855b2e054b470dcf10b459
amitpatel <amit.patel@vayana.com>
2024-08-13 11:31:39 +0530

    Minor changes

--- cic/users/tests/test_urls.py
+++ cic/users/tests/test_urls.py
@@ -1,16 +1,6 @@
 from django.urls import resolve
 from django.urls import reverse
 
-from cic.users.models import User
-
-
-def test_detail(user: User):
-    assert (
-        reverse("users:detail", kwargs={"username": user.username})
-        == f"/users/{user.username}/"
-    )
-    assert resolve(f"/users/{user.username}/").view_name == "users:detail"
-
 
 def test_update():
     assert reverse("users:update") == "/users/~update/"
